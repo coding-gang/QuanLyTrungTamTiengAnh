e7ab2fb11d38f4db68e5b5672b8b1a6aef777e11
Huynh Thien Tan <thientanhuynh189@gmail.com>
2022-04-12 22:23:51 +0700

    Remove button in top page

--- QuanLyTrungTamTiengAnh/UI/Form_Home.Designer.cs
+++ QuanLyTrungTamTiengAnh/UI/Form_Home.Designer.cs
@@ -35,11 +35,6 @@
             this.mnuThongke = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuBaocao = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuHotro = new System.Windows.Forms.ToolStripMenuItem();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.lblName = new Krypton.Toolkit.KryptonLabel();
-            this.btnKhoahoc = new FontAwesome.Sharp.IconButton();
-            this.btnDangky = new FontAwesome.Sharp.IconButton();
-            this.btnHocvien = new FontAwesome.Sharp.IconButton();
             this.kryptonStatusStrip1 = new Krypton.Toolkit.KryptonStatusStrip();
             this.tlbLabelTendangnhap = new System.Windows.Forms.ToolStripStatusLabel();
             this.tlbTendangnhap = new System.Windows.Forms.ToolStripStatusLabel();
@@ -62,7 +57,6 @@
             this.panel9 = new System.Windows.Forms.Panel();
             this.panel10 = new System.Windows.Forms.Panel();
             this.mnuHome.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.kryptonStatusStrip1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.tc1.SuspendLayout();
@@ -124,88 +118,6 @@
             this.mnuHotro.Size = new System.Drawing.Size(53, 20);
             this.mnuHotro.Text = "Hỗ trợ";
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.lblName);
-            this.panel1.Controls.Add(this.btnKhoahoc);
-            this.panel1.Controls.Add(this.btnDangky);
-            this.panel1.Controls.Add(this.btnHocvien);
-            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 24);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 36);
-            this.panel1.TabIndex = 1;
-            // 
-            // lblName
-            // 
-            this.lblName.Dock = System.Windows.Forms.DockStyle.Right;
-            this.lblName.Location = new System.Drawing.Point(670, 0);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(130, 36);
-            this.lblName.TabIndex = 3;
-            this.lblName.Values.Text = "Trần Đỗ Phương Nam";
-            // 
-            // btnKhoahoc
-            // 
-            this.btnKhoahoc.AutoSize = true;
-            this.btnKhoahoc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnKhoahoc.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnKhoahoc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnKhoahoc.IconChar = FontAwesome.Sharp.IconChar.BookOpen;
-            this.btnKhoahoc.IconColor = System.Drawing.Color.Black;
-            this.btnKhoahoc.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnKhoahoc.IconSize = 37;
-            this.btnKhoahoc.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.btnKhoahoc.Location = new System.Drawing.Point(220, 0);
-            this.btnKhoahoc.Name = "btnKhoahoc";
-            this.btnKhoahoc.Size = new System.Drawing.Size(110, 36);
-            this.btnKhoahoc.TabIndex = 2;
-            this.btnKhoahoc.Text = "Khóa học";
-            this.btnKhoahoc.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnKhoahoc.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnKhoahoc.UseVisualStyleBackColor = true;
-            // 
-            // btnDangky
-            // 
-            this.btnDangky.AutoSize = true;
-            this.btnDangky.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnDangky.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnDangky.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDangky.IconChar = FontAwesome.Sharp.IconChar.AddressCard;
-            this.btnDangky.IconColor = System.Drawing.Color.Black;
-            this.btnDangky.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnDangky.IconSize = 37;
-            this.btnDangky.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.btnDangky.Location = new System.Drawing.Point(110, 0);
-            this.btnDangky.Name = "btnDangky";
-            this.btnDangky.Size = new System.Drawing.Size(110, 36);
-            this.btnDangky.TabIndex = 1;
-            this.btnDangky.Text = "Đăng ký";
-            this.btnDangky.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDangky.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnDangky.UseVisualStyleBackColor = true;
-            // 
-            // btnHocvien
-            // 
-            this.btnHocvien.AutoSize = true;
-            this.btnHocvien.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnHocvien.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnHocvien.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnHocvien.IconChar = FontAwesome.Sharp.IconChar.GraduationCap;
-            this.btnHocvien.IconColor = System.Drawing.Color.Black;
-            this.btnHocvien.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnHocvien.IconSize = 37;
-            this.btnHocvien.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.btnHocvien.Location = new System.Drawing.Point(0, 0);
-            this.btnHocvien.Name = "btnHocvien";
-            this.btnHocvien.Size = new System.Drawing.Size(110, 36);
-            this.btnHocvien.TabIndex = 0;
-            this.btnHocvien.Text = "Học viên";
-            this.btnHocvien.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnHocvien.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnHocvien.UseVisualStyleBackColor = true;
-            // 
             // kryptonStatusStrip1
             // 
             this.kryptonStatusStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
@@ -264,9 +176,9 @@
             // 
             this.panel2.Controls.Add(this.tc1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 60);
+            this.panel2.Location = new System.Drawing.Point(0, 24);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 368);
+            this.panel2.Size = new System.Drawing.Size(800, 404);
             this.panel2.TabIndex = 3;
             // 
             // tc1
@@ -279,7 +191,7 @@
             this.tc1.Location = new System.Drawing.Point(0, 0);
             this.tc1.Name = "tc1";
             this.tc1.SelectedIndex = 0;
-            this.tc1.Size = new System.Drawing.Size(800, 368);
+            this.tc1.Size = new System.Drawing.Size(800, 404);
             this.tc1.TabIndex = 0;
             // 
             // tpHocvien
@@ -289,7 +201,7 @@
             this.tpHocvien.Location = new System.Drawing.Point(4, 22);
             this.tpHocvien.Name = "tpHocvien";
             this.tpHocvien.Padding = new System.Windows.Forms.Padding(3);
-            this.tpHocvien.Size = new System.Drawing.Size(792, 342);
+            this.tpHocvien.Size = new System.Drawing.Size(792, 378);
             this.tpHocvien.TabIndex = 0;
             this.tpHocvien.Text = "Học viên";
             this.tpHocvien.UseVisualStyleBackColor = true;
@@ -299,7 +211,7 @@
             this.panel7.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel7.Location = new System.Drawing.Point(396, 3);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(393, 336);
+            this.panel7.Size = new System.Drawing.Size(393, 372);
             this.panel7.TabIndex = 3;
             // 
             // panel8
@@ -307,7 +219,7 @@
             this.panel8.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel8.Location = new System.Drawing.Point(3, 3);
             this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(393, 336);
+            this.panel8.Size = new System.Drawing.Size(393, 372);
             this.panel8.TabIndex = 2;
             // 
             // tpDangky
@@ -403,15 +315,12 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.kryptonStatusStrip1);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.mnuHome);
             this.MainMenuStrip = this.mnuHome;
             this.Name = "frmHome";
             this.Text = "Form_Home";
             this.mnuHome.ResumeLayout(false);
             this.mnuHome.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.kryptonStatusStrip1.ResumeLayout(false);
             this.kryptonStatusStrip1.PerformLayout();
             this.panel2.ResumeLayout(false);
@@ -434,11 +343,6 @@
         private System.Windows.Forms.ToolStripMenuItem mnuThongke;
         private System.Windows.Forms.ToolStripMenuItem mnuBaocao;
         private System.Windows.Forms.ToolStripMenuItem mnuHotro;
-        private System.Windows.Forms.Panel panel1;
-        private FontAwesome.Sharp.IconButton btnHocvien;
-        private Krypton.Toolkit.KryptonLabel lblName;
-        private FontAwesome.Sharp.IconButton btnKhoahoc;
-        private FontAwesome.Sharp.IconButton btnDangky;
         private Krypton.Toolkit.KryptonStatusStrip kryptonStatusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel tlbLabelTendangnhap;
         private System.Windows.Forms.ToolStripStatusLabel tlbTendangnhap;
